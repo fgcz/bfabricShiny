c7622508499dd02407086cce814c7fbbc06aabab
christian <christian>
2016-08-30 09:27:27 +0200

    current version

--- demo/server.R
+++ demo/server.R
@@ -10,17 +10,36 @@
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
    
-  output$distPlot <- renderPlot({
+  
+      # Combine the selected variables into a new data frame
+      selectedData <- reactive({
+      qcdata[, c(input$xcol, input$ycol)]
+       })
+  
+  
+  
+  
+
+    output$plot1 <- renderPlot({
+      par(mar = c(5.1, 4.1, 0, 1))
+      plot(selectedData(),
+           pch = 20, cex = 1)
+      abline(h = input$MS1, col = "red")
+     
     
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #x    <- faithful[, 2] 
+    #myCol <- input$dataset
+    #x    <- state.x77[, which(colnames(state.x77) == input$dataset)]
+    #bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    #hist(x, breaks = bins, col = 'darkgray', border = 'white', main = input$dataset)
     
   })
   
 })
+
+
--- demo/ui.R
+++ demo/ui.R
@@ -8,26 +8,42 @@
 #
 
 library(shiny)
+qcdata <-read.csv("C:/Users/christian/Documents/Learning_R_scripts/Testdata_Raw_analyzer/Bernd/20160409_05_GM_06.csv")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("ChT test plot"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
+      # sliderInput("bins",
+      #             "Number of bins:",
+      #             min = 1,
+      #             max = 50,
+      #             value = 30),
+      # sliderInput("bins2",
+      #             "Number of bins:",
+      #             min = 1,
+      #             max = 50,
+      #             value = 30),
+      # selectInput("dataset", "Choose a dataset:", 
+      #             choices = colnames(state.x77)),
+    
+    selectInput('xcol', 'X Variable', names(qcdata)),
+    
+    selectInput('ycol', 'Y Variable', names(qcdata),
+                selected=names(qcdata)[[2]]),
+    
+    numericInput('MS1', 'MS1', 64,
+                  min = 32, max = 256, step = 32)
+  ),
     
     # Show a plot of the generated distribution
     mainPanel(
-       plotOutput("distPlot")
+       plotOutput("plot1")
     )
   )
 ))
