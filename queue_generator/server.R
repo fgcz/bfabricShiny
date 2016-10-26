6cf38b2b4208bf5e86d9207a77aaefd6c6765a1d
Christian Panse <cp@fgcz.ethz.ch>
2016-10-26 07:36:28 +0200

    working with output table

--- queue_generator/server.R
+++ queue_generator/server.R
@@ -9,47 +9,69 @@ library(shiny)
 library(jsonlite)
 
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
 
+  
+  
+  
   output$project <- renderUI({
-    res.project <- c(1000, 1959, 2121)
-    selectInput('project', 'Project:', res.project, multiple = FALSE, selected = 1000)
+    res.project <- c(NA, 1000, 1959, 2121)
+    selectInput('project', 'Project:', res.project, multiple = FALSE, selected = NA)
   })
   
   
   output$sample <- renderUI({
-    res <- as.data.frame(fromJSON(paste("http://localhost:5000/projectid/", input$project, sep='')))
-    selectInput('sample', 'Sample:', paste(res$sample.id, res$sample.name, sep='-'), multiple = TRUE)
+    if ( input$project %in% 1000:2500){
+      res <- as.data.frame(fromJSON(paste("http://localhost:5000/projectid/", input$project, sep='')))
+      selectInput('sample', 'Sample:', paste(res$sample.id, res$sample.name, sep='-'), multiple = TRUE)
+      
+    }else{
+      selectInput('sample', 'Sample:', NULL)
+    }
+  })
+  
+  
+  getExtracts <- reactive({
+    sample.id <- sapply(strsplit(input$sample, split='-'), function(x){x[1]})
+    res <- do.call('rbind', 
+                   lapply(sample.id, 
+                          function(sampleid){
+                            as.data.frame(fromJSON(paste("http://localhost:5000/sampleid/", sampleid, sep='')))
+                          }))
   })
   
   output$extract <- renderUI({
 
-    if (length(input$sample) > 0){
-      sample.id <- sapply(strsplit(input$sample, split='-'), function(x){x[1]})
-      
-      
-      res <- do.call('rbind', 
-                     lapply(sample.id, 
-                            function(sampleid){
-                              as.data.frame(fromJSON(paste("http://localhost:5000/sampleid/", sampleid, sep='')))
-                              }))
-      
-
-        
-      selectInput('extract', 'Extract:', res$extract.name, multiple = TRUE)
+    if (input$sample == "" || length(input$sample) == 0 || is.null(input$sample)){
+      selectInput('extract', 'Extract:', NULL)
     }   else{
-     selectInput('extract', 'Extract:', "NA")
+      
+      res <- getExtracts()
+      selectInput('extract', 'Extract:', res$extract.name, multiple = TRUE)  
+       
     }
   })
   
   
   
   output$table <- DT::renderDataTable(DT::datatable({
+  
+    
     
     # call Christian Trachsel's code here!
-    data <- iris
+ 
+   
     
-    data
-  }))
+    print (input$extract)
+    if (input$extract != "" && length(input$extract)>0){
+      res <- getExtracts()
+      idx <- res$extract.name %in% input$extract
+      res[idx, ]
+    }else{
+      #as.data.frame(list(extract.name=NA, sampleid=NA, extract.id=NA))
+      as.data.frame(list(output="no data yet"))
+    }
+    
+    }))
 
 })
