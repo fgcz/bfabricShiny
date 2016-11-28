deb7187c5ebb1be34505571523cf7462deeedce8
Christian Trachsel <christian.trachsel@fgcz.ethz.ch>
2016-11-28 15:19:25 +0100

    updated random queue generator

--- queue_generator/compose_queue.R
+++ queue_generator/compose_queue.R
@@ -16,15 +16,17 @@ InsertFetuin <- function(df, group, rowsneeded)
   }))
 }
 
-#helper function1a
-format_input_data <- function(data, multiple = 2, hplc = ""){
+#helper function1
+.format_input_data <- function(data, multiple = 2, hplc = ""){
   n <- nrow(data)
   res <- data
   if (hplc == "eksigent") {
     pos <- (paste(rep(2, times = n), paste(rep(LETTERS[1:6], each = 8)[1:n], rep(sprintf("%02d", c(1:8)), times = 8)[1:n], sep = ""), sep = "")) #generates plate position in eksigent format for a 48 position plate (A1-F8)
-  } else if ( hplc == "waters") {
+  } else if (hplc == "waters") {
     pos <- paste(rep(1, times = n), paste(rep(LETTERS[1:6], each = 8)[1:n], rep(1:8, times = 8)[1:n], sep = ","), sep = ":") #generates plate position in waters format for a 48 position plate (A1-F8)
     pos <- paste0('"', pos, '"')
+  } else if (hplc == "easylc"){
+    pos <- paste(rep(LETTERS[1:6], each = 8)[1:n], rep(1:8, times = 8)[1:n], sep = "") #generates plate position in thermo easyLC format for a 48 position plate (A1-F8)
   } else {
     pos <- paste(rep(LETTERS[1:6], each = 8)[1:n], rep(1:8, times = 8)[1:n], sep = "") #generates plate position in thermo easyLC format for a 48 position plate (A1-F8)
   }
@@ -36,9 +38,42 @@ format_input_data <- function(data, multiple = 2, hplc = ""){
   } else {
     
   }
-  assign("res", res, .GlobalEnv) #only for development purpose! remove from active code
-  #res
-}  
+    res
+} 
+
+#helper function 2
+.generate_template_random_multiple <- function(res, how.often = 2, how.many = 1, hplc = ""){
+  n <- nrow(res)
+  repeats <- length(unique(res$Condition))
+  cond <- max(table(res$Condition))
+  res <- FUN(res)
+  condition.vector <- as.vector(replicate(repeats, 1:cond))
+  blockgroupindex <- which(colnames(res) == "Condition")[1]
+  res <- InsertFetuin(res, blockgroupindex, cond)
+  res["blockrandom"] <- condition.vector
+  res <- res[order(rank(res$blockrandom)),]
+  res$blockrandom <- NULL
+  m <- nrow(res)
+  blocks <- rep(1:((m/how.often)+1), each = how.often)[1:m] #generates an index among which the dataset will be extended
+  res["blocks"] <- blocks #attache the index to the data
+  groupindex <- which(colnames(res) == "blocks")[1] #fetch the index of the column containing the block information
+  rowsneeded <- how.often + how.many #define by how many row each block will be extended
+  res[sapply(res, is.factor)] <- lapply(res[sapply(res, is.factor)], as.character)
+  res$extract.name[is.na(res$extract.name)] <- "tobedeleted"
+  res <- InsertFetuin(res, groupindex, rowsneeded) #extend the sample list by empty rows
+  res[sapply(res, is.factor)] <- lapply(res[sapply(res, is.factor)], as.character) #convert all Factors to characters
+  if (hplc == "easylc"){
+    res$Position[is.na(res$Position)] <- "F8"
+  } else if ( hplc == "waters"){
+    res$Position[is.na(res$Position)] <- '"1:F,8"'
+  } else {
+    res$Position[is.na(res$Position)] <- "1F08"
+  }
+  res$extract.name[is.na(res$extract.name)] <- "Fetuin_400amol"
+  res$Condition[is.na(res$Condition)] <- "Fetuin"
+  res <- res[-which(res$extract.name == "tobedeleted"),]  
+    res
+}
 
 .generate_template_base <- function(data, how.often = 2, how.many = 1){
   n <- nrow(data)
@@ -92,14 +127,15 @@ format_input_data <- function(data, multiple = 2, hplc = ""){
 }
 
 generate_queue <- function(data, foldername='',projectid=1000, area='Proteomics', instrument='NA', username='cpanse', how.often=2, how.many=1){
-  res.1 <- .generate_template_base(data, how.often, how.many)
-  res.2 <- .generate_folder_name(foldername=foldername, area=area, instrument=instrument, username=username, res=res.1)
-  res.3 <- .generate_name(res=res.1)
+  res.1 <- .format_input_data(data, multiple, hplc)
+  res.2 <- .generate_template_base(res=res.1, how.often, how.many, hplc)
+  res.3 <- .generate_folder_name(foldername=foldername, area=area, instrument=instrument, username=username, res=res.2)
+  res.4 <- .generate_name(res=res.2)
   #queue <- data.frame(cbind(nam, out, res$Position, InjVol))
   #colnames(queue) <- c("File Name", "Path", "Position", "Inj Vol")
-  cbind('File Name' = res.3,
-        'Path' = paste("D:\\Data2San", paste('p',projectid,sep=''), res.2, sep="\\"), 
-        'Position' = res.1$Position,
+  cbind('File Name' = res.4,
+        'Path' = paste("D:\\Data2San", paste('p',projectid,sep=''), res.3, sep="\\"), 
+        'Position' = res.2$Position,
         'Inj Vol' = 2
         )
 }
