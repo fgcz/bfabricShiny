2ef13f077799971d5ef40d285f2f08648ebe369a
Christian Panse <cp@fgcz.ethz.ch>
2017-05-10 15:19:16 +0200

    initial fgcz.R

---
+++ R/fgcz.R
@@ -0,0 +1,34 @@
+#R
+
+
+
+.workunit2resource <- function(workunit_id = 153887){
+  
+  query_url <- paste("http://localhost:5000/zip_resource_of_workunitid/", workunit_id, sep='')
+
+  res <- as.data.frame(fromJSON(query_url))
+
+  return(res)
+
+}
+
+.unzip <- function(zipfile=NULL, file=NULL){
+  cmd <- paste('unzip -p ', zipfile, file)
+  content <- read.csv(pipe(cmd), sep='\t', stringsAsFactors = FALSE, header = TRUE)
+  content
+}
+
+.ssh_unzip <- function(host = 'fgcz-r-021.uzh.ch', user = 'cpanse', zipfile = .workunit2resource(), file = 'proteinGroups.txt'){
+  
+  cmd <- paste('unzip -p ', zipfile, file)
+  ssh_cmd <- paste("ssh ", user, "@", host, " '", cmd, "'", sep="")
+  message(ssh_cmd)
+
+  S <- read.csv(pipe(ssh_cmd), sep='\t', stringsAsFactors = FALSE, header = TRUE)
+
+  S
+}
+
+.getMaxQuantFilesNames <- function(S){
+  gsub("Intensity\\.", "", grep("Intensity\\.",colnames(S),value=T) )
+}
