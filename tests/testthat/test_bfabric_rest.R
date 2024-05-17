99a013a3b29d483647b228e6c6962480f9ffcc49
Leonardo Schwarz <leonardo.schwarz@fgcz.ethz.ch>
2024-05-17 14:50:45 +0200

    fix some data dependent tests

--- tests/testthat/test_bfabric_rest.R
+++ tests/testthat/test_bfabric_rest.R
@@ -50,15 +50,14 @@ test_that("test read", {
   resources <- bfabricShiny:::.getResources(login,
                                             webservicepassword,
                                             posturl = bfabricposturl,
-                                            workunitid = 187604) |>
-    sapply(function(x)x[['_id']])
-
-
+                                            workunitid = 187604,
+                                            createdbefore = "2020-02-10 16:01:00") |>
+    sapply(function(x)x[['id']])
 
 
 
   # will fail once someone adds a WU to p3000
-  expect_true(sum(
+  expect_equal(
     c(1527155, 1527154, 1527153, 1527152, 1498605, 1498595, 1498589, 1498588,
     1498433, 1498199, 1457753, 1457752, 1457751, 1441556, 1441552, 1404513,
     1404508, 1404501, 1404498, 1401796, 1401771, 1394399, 1393917, 1393914,
@@ -71,7 +70,8 @@ test_that("test read", {
     1326611, 1326607, 1322571, 1321363, 1320383, 1304484, 1299985, 1299984,
     1299983, 1297159, 1297157, 1284306, 1284209, 1284193, 1284190, 1284182,
     1283003, 1282997, 1282995, 1282871, 1282799, 1282798, 1282797, 1282796,
-    1282795, 1279978, 1279940, 1279828) %in% resources) == 100)
+    1282795, 1279978, 1279940, 1279828), resources
+  )
 
   expect_true(bfabricShiny::readPages(endpoint = 'application',
                                       query = list('id' = 19),
@@ -81,15 +81,17 @@ test_that("test read", {
 
 
   expect_true("202584 - MaxQuant_Abird_2ng" %in%
-                bfabricShiny:::.getWorkunits(login,
-                                             webservicepassword,
-                                             posturl = bfabricposturl,
-                                             containerid = 3000))
+              bfabricShiny:::.getWorkunits(login,
+                                           webservicepassword,
+                                           posturl = bfabricposturl,
+                                           containerid = 3000,
+                                           createdbefore = "2019-10-26 15:26:49"))
 
-  expect_null(bfabricShiny:::.getWorkunits(login,
-                                              webservicepassword,
-                                             posturl = bfabricposturl,
-                                             containerid = 3001))
+  expect_equal(list(), bfabricShiny:::.getWorkunits(login,
+                                           webservicepassword,
+                                           posturl = bfabricposturl,
+                                           containerid = 3001,
+                                           createdbefore = "2019-10-26 15:26:49"))
 
 
   expect_true(length(bfabricShiny:::.getApplications(login, webservicepassword,
