7bb8c6197313940a90428adadbcda8352648b44a
Christian Panse <cp@fgcz.ethz.ch>
2025-07-31 12:30:50 +0200

    enable iRT tab

--- inst/shiny/bfabric_rawDiag2/app.R
+++ inst/shiny/bfabric_rawDiag2/app.R
@@ -9,6 +9,48 @@ stopifnot(
   packageVersion('bfabricShiny') >= "0.13.4",
   packageVersion('rawDiag') >= "1.3")
 
+rawrrrUI <- function(id){
+  htmlOutput(NS(id, "plot"))
+}
+
+rawrrrServer <- function(id, rawfiles, msg){
+  moduleServer(id,
+               function(input, output, session) {
+                 rawrrrServerReady <- reactiveVal(FALSE)
+                 
+                 observeEvent(rawfiles(), {
+                   if(length(rawfiles()) > 0 && !is.null(rawfiles())){
+                     
+                     rawfiles() |> 
+                       lapply(FUN = function(x){
+                         fgczqcms::rawrrServer(id = paste0("rawrrr-", x),
+                                               vals = reactiveValues(fn = x, mZ = fgczqcms:::.iRTmz()),
+                                               msg = msg)
+                       })
+                     
+                     rawrrrServerReady(TRUE)
+                   }
+                 })
+
+                 
+                 output$plot <- renderUI({
+                   shiny::req(rawrrrServerReady())
+                   
+                   tryCatch({
+                     rawfiles() |>
+                       lapply(FUN = function(x) {
+                         fgczqcms::rawrrUI(session$ns(paste0("rawrrr-", x)))
+                       })}, error = function(e) {
+                         message("Error in module rawrrr renderUI: ", e$message)
+                         list()  # Return an empty list in case of error
+                       }) -> plot_output_list
+                   
+                   # Convert the list to a tagList - this is necessary for the list of items
+                   # to display properly.
+                   do.call(tagList, plot_output_list)
+                 })
+               })
+}
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -29,18 +71,14 @@ ui <- fluidPage(
     ),
     mainPanel(
       bslib::navset_card_underline(
-        
-        #bslib::nav_panel("Biognosys iRT XICs",  htmlOutput("rawrr")),
-        bslib::nav_panel("rawDiag", rawDiag::rawDiagUI("OrbitrapFun02")),
-        #  )
+        bslib::nav_panel("rawDiag", htmlOutput("rawDiag")),
+        bslib::nav_panel("Biognosys iRT XIC profiles",  htmlOutput("rawrrr")),
       )
     )
   )
 )
 
-
 server <- shinyServer(function(input, output, session) {
-  
   vals <- reactiveValues(
     generatePDF = 0,
     rawfile  = NULL,
@@ -96,45 +134,36 @@ server <- shinyServer(function(input, output, session) {
                })
   
   msg <- reactiveVal("starting rawDiag shiny application ...")
+  rawrrrServerReady <- reactiveVal(FALSE)
   
   observeEvent(msg(), {
     shiny::showNotification(msg(), type = "message", duration = 3)
     cat(msg())
   })
   
-#  observeEvent(vals$rawfile, {
-#    if(length(vals$rawfile) > 0 && !is.null(vals$rawfile)){
-#      
-#      vals$rawfile |> 
-##        lapply(FUN = function(x){
-#          fgczqcms::rawrrServer(id = x,
-#                                vals = reactiveValues(fn = x, mZ = fgczqcms:::.iRTmz()),
-#                                msg = msg)
-#        })
-#    }
-#  })
-  
+  ### ==== rawrr UI / Servers ====
   observeEvent(vals$rawfile, {
     if(length(vals$rawfile) > 0 && !is.null(vals$rawfile)){
-      rawDiag:::rawDiagServer("OrbitrapFun02", vals)
+      rawrrrServer("Orbitrap-rawrr", rawfiles = reactiveVal(vals$rawfile), msg = msg)
+      rawrrrServerReady(TRUE)
     }
   })
   
+  output$rawrrr <- renderUI({
+    rawrrrUI(session$ns("Orbitrap-rawrr"))
+  })
   
-#  output$rawrr <- renderUI({
-#    shiny::req(vals$rawfile)
-#    if(length(vals$rawfile) > 0 && !is.null(vals$rawfile)){
-#      vals$rawfile |> 
-#        lapply(FUN = function(x){
-#          tagList(
-#            fgczqcms::rawrrUI(x),
-#          )
-##        }
-#        )
-#    }else{
-#      h2("no files provided.")
-#    }
-#  })
+  ### ==== rawDiag UI / Servers ====
+  observeEvent(vals$rawfile, {
+    shiny::req(rawrrrServerReady())
+    if(length(vals$rawfile) > 0 && !is.null(vals$rawfile)){
+      rawDiag:::rawDiagServer("Orbitrap-rawDiag", vals)
+    }
+  })
+  
+  output$rawDiag <- renderUI({
+    rawDiag::rawDiagUI(session$ns("Orbitrap-rawDiag"))
+  })
   
   bfabricUpload <- observeEvent(input$generate, {
     
